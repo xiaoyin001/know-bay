bae660fdb3748f3dfb0fbd753b41b9db28e03664
xiaoyin <1305794194@qq.com>
2025-04-09 17:03:54 +0800

    [Update] 路径变更

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import movezeroes "xy/leetcode/top-100-liked/hash/283-move-zeroes"
+import movezeroes "xy/leetcode/top-100-liked/double-point/283-move-zeroes"
 
 func main() {
 	// twosum.TwoSumTest()
